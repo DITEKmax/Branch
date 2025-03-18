4b6fd31137964b3f6a2f6a3edf1b3584bc6fdaeb
DITEKmax <maks.ditkovskiy.05@mail.ru>
2025-03-18 16:31:10 +0300

    Добавлена проверка на отрицательные числа в addNumbers

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ class Main {
     public static void main(String[] args) {
         System.out.println("Это добавлено в ветке main");
         greetInDifferentLanguages();
-        int sum = addNumbers(5, 10);
+        int sum = addNumbers(-5, 10);
         System.out.println("Сумма чисел: " + sum);
     }
 
@@ -14,6 +14,10 @@ class Main {
     }
 
     public static int addNumbers(int a, int b) {
+        if (a < 0 || b < 0) {
+            System.out.println("Ошибка: Одно из чисел отрицательное");
+            return -1;
+        }
         return a + b;
     }
 }
