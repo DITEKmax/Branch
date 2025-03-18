5212dd3c6f60733f9caff46753ee37ce927bfe29
DITEKmax <maks.ditkovskiy.05@mail.ru>
2025-03-18 16:27:28 +0300

    Добавлена функция greetInDifferentLanguages

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
-class Main{
+class Main {
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке main");
+        greetInDifferentLanguages();
+    }
+
+    public static void greetInDifferentLanguages() {
+        System.out.println("\nПриветствие на разных языках:");
+        System.out.println("English: Hello!");
+        System.out.println("Español: ¡Hola!");
+        System.out.println("Français: Bonjour!");
     }
 }
